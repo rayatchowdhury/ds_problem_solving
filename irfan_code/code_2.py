ad4508a3ecd73fa198456682d5114a5a5d70bbb1
greekmonkey123 <rahmaniirfanur@gmail.com>
2026-02-15 12:23:45 -0400

    code 2

---
+++ irfan_code/code_2.py
@@ -0,0 +1,18 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    used = set()
+    count = 0
+    
+    for x in arr:
+        if x not in used:
+            used.add(x)
+            count += 1
+        elif x + 1 not in used:
+            used.add(x + 1)
+            count += 1
+    
+    print(count)
